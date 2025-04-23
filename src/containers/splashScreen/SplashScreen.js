bd30b168935a4090a3573f39e962a27d542723dc
btthanh <btthanh@cmcglobal.vn>
2025-04-23 08:50:35 +0700

    init commit

---
+++ src/containers/splashScreen/SplashScreen.js
@@ -0,0 +1,24 @@
+import React, {useContext} from "react";
+import "./SplashScreen.css";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import {greeting, splashScreen} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function SplashScreen() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <div className={isDark ? "dark-mode splash-container" : "splash-container"}>
+      <div className="splash-animation-container">
+        <DisplayLottie animationData={splashScreen.animation} />
+      </div>
+      <div className="splash-title-container">
+        <img
+          src={require("../../assets/images/logo.jpeg")}
+          alt="Logo"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  );
+}
